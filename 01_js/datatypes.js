d325ac5065c305ae9a8e6e9e3f79964c19919b20
ahmadatoms <112195597+ahmadatoms@users.noreply.github.com>
2023-09-23 10:06:55 +0000

    datatypes

---
+++ 01_js/datatypes.js
@@ -0,0 +1,19 @@
+//alert(3+3)//we are using node js not browser
+let name = "ahmad"
+let age = 20
+let isloggedin = false
+
+
+//primitives datatypes
+//number = 2 to power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined = 
+//symbol = unique
+// null is a object
+
+
+//object
+console.log(typeof age)
