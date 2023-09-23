6baf0014e8b0e118929761e9510f4f58f78ad546
ahmadatoms <112195597+ahmadatoms@users.noreply.github.com>
2023-09-23 10:31:02 +0000

    typeconversion

---
+++ 01_js/typeconversion.js
@@ -0,0 +1,14 @@
+let score = "33"
+console.log(typeof score)
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber)
+
+//boolean conversion
+let logged = 1
+let boollogged = Boolean(logged)
+console.log(boollogged)
+//number to string
+let setnumber = 33
+let stringnum = String(setnumber)
+console.log(stringnum)
+console.log(typeof stringnum)
