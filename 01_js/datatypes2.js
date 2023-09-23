3414ecb45fc573474e6eded3609a6551b8e610a8
ahmadatoms <112195597+ahmadatoms@users.noreply.github.com>
2023-09-23 15:22:46 +0000

    datatypes

---
+++ 01_js/datatypes2.js
@@ -0,0 +1,26 @@
+//primitives data types
+//null,boolean,number ,string,undefined
+//reference(non primitive) 
+//array object functions
+
+
+//arrays
+const heros = ["iron man ","super man","shakti man"];
+console.log(heros)
+console.log(typeof heros)
+
+
+//object
+let myObj = {
+    name: "Ahmad",
+    age : 20
+}
+console.log(myObj)
+console.log(typeof myObj)
+
+
+
+// function inside variable 
+const myFunction = function(){
+    console.log("i will win this world")
+}
