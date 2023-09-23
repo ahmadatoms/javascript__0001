a7b36ca255d268abda7d7021e3190501b3c45cdd
ahmadatoms <112195597+ahmadatoms@users.noreply.github.com>
2023-09-23 11:00:14 +0000

    again

--- 01_js/typeconversion.js
+++ 01_js/typeconversion.js
@@ -12,3 +12,19 @@ let setnumber = 33
 let stringnum = String(setnumber)
 console.log(stringnum)
 console.log(typeof stringnum)
+
+
+
+// string add and operators
+
+let str1 = "hello"
+let str2 = " ahmad"
+let str3 = str1+str2
+console.log(str3)
+
+
+//confusing factors
+console.log("1" + 2)
+console.log("2" + 1)
+console.log("1" +2 +2)
+console.log(1+2+ "2")
