85001db36134b72a94ab743dbae3f83edd34f1a2
ahmadatoms <112195597+ahmadatoms@users.noreply.github.com>
2023-09-24 11:22:59 +0000

    strings

--- 01_js/strings.js
+++ 01_js/strings.js
@@ -20,3 +20,20 @@ console.log(newSTRING2)
 console.log(newSTRING2.trim())
  //convert string to arrays 
  console.log(gameName.split('_'))
+
+
+ // maths in javascript
+ console.log(Math)
+ console.log(Math.abs(-4))
+ console.log(Math.round(4.6))
+ console.log(Math.ceil(4.3))
+ console.log(Math.floor(4.2))
+ console.log(Math.min(4,5,6,78,7))
+
+ console.log(Math.random())
+ console.log(Math.random()*10)
+
+
+ const min = 10
+ const max= 20
+console.log(Math.floor(Math.random() *(max - min +1)) + min )
